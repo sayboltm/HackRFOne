ba8f7a667d00c8c6c01df99e2e54d400155e5825
Michael Saybolt <msaybolt@gmail.com>
2018-10-04 23:35:36 -0400

    Started M. Ossman Lesson 1

--- 1_FM_Radio/top_block.py
+++ 1_FM_Radio/top_block.py
@@ -1,11 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 ##################################################
-# Gnuradio Python Flow Graph
+# GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Tue Oct 24 16:24:38 2017
+# Generated: Tue Oct 24 20:48:19 2017
 ##################################################
 
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import blocks
 from gnuradio import eng_notation
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
@@ -16,7 +31,9 @@ from gnuradio.wxgui import forms
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import osmosdr
+import time
 import wx
+
 
 class top_block(grc_wxgui.top_block_gui):
 
@@ -30,12 +47,37 @@ class top_block(grc_wxgui.top_block_gui):
         ##################################################
         self.variable_slider_0 = variable_slider_0 = 50
         self.samp_rate = samp_rate = 1e6
+        self.channel_width = channel_width = 200e3
         self.channel_freq = channel_freq = 96.5e6
         self.center_freq = center_freq = 97.9e6
+        self.audio_gain = audio_gain = 1
 
         ##################################################
         # Blocks
         ##################################################
+        _audio_gain_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._audio_gain_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_audio_gain_sizer,
+        	value=self.audio_gain,
+        	callback=self.set_audio_gain,
+        	label='audio_gain',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._audio_gain_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_audio_gain_sizer,
+        	value=self.audio_gain,
+        	callback=self.set_audio_gain,
+        	minimum=0,
+        	maximum=2,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_audio_gain_sizer)
         self.wxgui_fftsink2_0_0 = fftsink2.fft_sink_c(
         	self.GetWin(),
         	baseband_freq=center_freq,
@@ -52,6 +94,22 @@ class top_block(grc_wxgui.top_block_gui):
         	peak_hold=False,
         )
         self.Add(self.wxgui_fftsink2_0_0.win)
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
+        	self.GetWin(),
+        	baseband_freq=channel_freq,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.Add(self.wxgui_fftsink2_0.win)
         _variable_slider_0_sizer = wx.BoxSizer(wx.VERTICAL)
         self._variable_slider_0_text_box = forms.text_box(
         	parent=self.GetWin(),
@@ -75,6 +133,12 @@ class top_block(grc_wxgui.top_block_gui):
         	proportion=1,
         )
         self.Add(_variable_slider_0_sizer)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=12,
+                decimation=5,
+                taps=None,
+                fractional_bw=None,
+        )
         self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
         self.osmosdr_source_0.set_sample_rate(samp_rate)
         self.osmosdr_source_0.set_center_freq(center_freq, 0)
@@ -88,13 +152,29 @@ class top_block(grc_wxgui.top_block_gui):
         self.osmosdr_source_0.set_antenna("", 0)
         self.osmosdr_source_0.set_bandwidth(0, 0)
           
+        self.low_pass_filter_0 = filter.fir_filter_ccf(int(samp_rate/channel_width), firdes.low_pass(
+        	1, samp_rate, 75e3, 25e3, firdes.WIN_HAMMING, 6.76))
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((audio_gain, ))
+        self.audio_sink_0 = audio.sink(48000, "", True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=480e3,
+        	audio_decimation=10,
+        )
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, center_freq-channel_freq, 1, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.osmosdr_source_0, 0), (self.wxgui_fftsink2_0_0, 0))
-
-
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))    
+        self.connect((self.analog_wfm_rcv_0, 0), (self.blocks_multiply_const_vxx_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))    
+        self.connect((self.blocks_multiply_xx_0, 0), (self.wxgui_fftsink2_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.rational_resampler_xxx_0, 0))    
+        self.connect((self.osmosdr_source_0, 0), (self.blocks_multiply_xx_0, 0))    
+        self.connect((self.osmosdr_source_0, 0), (self.wxgui_fftsink2_0_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_rcv_0, 0))    
 
     def get_variable_slider_0(self):
         return self.variable_slider_0
@@ -109,34 +189,51 @@ class top_block(grc_wxgui.top_block_gui):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 75e3, 25e3, firdes.WIN_HAMMING, 6.76))
         self.osmosdr_source_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
         self.wxgui_fftsink2_0_0.set_sample_rate(self.samp_rate)
+
+    def get_channel_width(self):
+        return self.channel_width
+
+    def set_channel_width(self, channel_width):
+        self.channel_width = channel_width
 
     def get_channel_freq(self):
         return self.channel_freq
 
     def set_channel_freq(self, channel_freq):
         self.channel_freq = channel_freq
+        self.analog_sig_source_x_0.set_frequency(self.center_freq-self.channel_freq)
+        self.wxgui_fftsink2_0.set_baseband_freq(self.channel_freq)
 
     def get_center_freq(self):
         return self.center_freq
 
     def set_center_freq(self, center_freq):
         self.center_freq = center_freq
+        self.analog_sig_source_x_0.set_frequency(self.center_freq-self.channel_freq)
         self.osmosdr_source_0.set_center_freq(self.center_freq, 0)
         self.wxgui_fftsink2_0_0.set_baseband_freq(self.center_freq)
 
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
-    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
-    (options, args) = parser.parse_args()
-    tb = top_block()
+    def get_audio_gain(self):
+        return self.audio_gain
+
+    def set_audio_gain(self, audio_gain):
+        self.audio_gain = audio_gain
+        self.blocks_multiply_const_vxx_0.set_k((self.audio_gain, ))
+        self._audio_gain_slider.set_value(self.audio_gain)
+        self._audio_gain_text_box.set_value(self.audio_gain)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    tb = top_block_cls()
     tb.Start(True)
     tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
